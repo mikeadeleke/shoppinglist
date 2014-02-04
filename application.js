9ed6b323b07e822e3fde229907bb6378056fcba2
Mike Adeleke <madeleke13@gmail.com>
2014-02-03 21:02:37 -0600

    more updates

--- application.js
+++ application.js
@@ -2,14 +2,13 @@ function addText() {
   var input = document.getElementById('input').value;
   if (input !== "string") {
     alert("Come on, please enter some valid text!");
-
+    return;
   }
 
   var addItem = document.createElement("li");
-  var css = "{type: checkbox}";
   var processItem = document.createTextNode(input);
   addItem.appendChild(processItem);
-  document.getElementById("todo-list").appendChild(addItem).appendChild(document.cssText(css));
+  document.getElementById("todo-list").appendChild(addItem);
 }
 
 
