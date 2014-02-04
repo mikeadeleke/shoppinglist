48c51fea75d02054dbdf1b20dd49686fafe0ffc0
Mike Adeleke <madeleke13@gmail.com>
2014-02-03 21:17:58 -0600

    added input validations

--- application.js
+++ application.js
@@ -1,17 +1,16 @@
 function addText() {
   var input = document.getElementById('input').value;
-  if (input !== "string") {
+  if (!input || 0 === input.length || /^\s*$/.test(input) ) {
     alert("Come on, please enter some valid text!");
     return;
   }
-
+  checkbox = document.createElement('input="checkbox"')
+  list_item = document.getElementById("todo-list")
   var addItem = document.createElement("li");
   var processItem = document.createTextNode(input);
   addItem.appendChild(processItem);
   document.getElementById("todo-list").appendChild(addItem);
+  addItem.insertBefore(checkbox, list_item)
 }
-
-
-
 
 
