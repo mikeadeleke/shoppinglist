b7daf364f863d085c4a8d6878bae510fe33e14f3
Mike Adeleke <madeleke13@gmail.com>
2014-02-04 18:03:10 -0600

    updated js

--- application.js
+++ application.js
@@ -1,16 +1,20 @@
 function addText() {
   var input = document.getElementById('input').value;
-  if (!input || 0 === input.length || /^\s*$/.test(input) ) {
+  if (!input || 0 === input.length || /^\s*$/.test(input)) {
     alert("Come on, please enter some valid text!");
     return;
   }
-  checkbox = document.createElement('input="checkbox"')
-  list_item = document.getElementById("todo-list")
+
+  var checkbox = document.createElement("input").type = "checkbox";
+  var list_item = document.getElementById("todo-list");
   var addItem = document.createElement("li");
   var processItem = document.createTextNode(input);
   addItem.appendChild(processItem);
   document.getElementById("todo-list").appendChild(addItem);
-  addItem.insertBefore(checkbox, list_item)
+  addItem.insertBefore(checkbox, list_item);
 }
 
+function checkItem() {
+  var listItem = document.getElementById("todo-list");
 
+}
